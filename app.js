2c02506e1b52cbfc72bdaeabd8986abdf8db3064
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-04 21:20:11 +0300

    feat: gpt model text-davinci-003

--- app.js
+++ app.js
@@ -9,13 +9,42 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-bot.on('message', async(msg) => {
-  const text = msg.text;
-  const chatId = msg.chat.id;
+const start = () => {
+  bot.setMyCommands([
+    {
+      command: '/start',
+      description: 'Начать заново',
+    },
+    {
+      command: '/info',
+      description: 'Информация о пользователе',
+    },
+  ]);
+};
+
+bot.on('message', async (msg) => {
+  const { text, chat } = msg;
+  const chatId = chat.id;
   const completion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: 'Hello world',
+    prompt: `${text}.\n`,
+    temperature: 1,
+    max_tokens: 4000,
   });
+  const output = completion.data.choices.pop();
 
-  return bot.sendMessage(chatId, 'test');
+  if (text === '/start') {
+    await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/22c/b26/22cb267f-a2ab-41e4-8360-fe35ac048c3b/7.webp');
+    return bot.sendMessage(chatId, `Добро пожаловать, ${msg.from.first_name}. Введите ваш вопрос, чтобы получить ответ от нейросети`);
+  }
+  if (text === '/info') {
+    return bot.sendMessage(
+      chatId,
+      `Ваше имя: ${msg.from.first_name}\nВаш никнейм: ${msg.from.username}`,
+    );
+  }
+
+  return bot.sendMessage(chatId, output.text);
 });
+
+start();
